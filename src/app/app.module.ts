68e5d9decfa41d9c8e596a113b2dd4e33d7ada83
Felix Cagatin <cagatingilbert@gmail.com>
2022-12-27 08:03:38 -0800

    Added basic skeleton structure for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
